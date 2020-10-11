13ca39307b5e211904b09719669a2b00ecb04f5b
Hossein Keshavarz <hosseinkeshavarz1997@gmail.com>
2020-10-11 23:42:47 +0330

    add astminer

---
+++ ast/code/example.py
@@ -0,0 +1,9 @@
+from sklearn.linear_model import LogisticRegression
+
+
+class LogisticRegressionModel:
+    def __init__(self, train_inputs):
+        self.train_inputs = train_inputs
+
+        self.model = LogisticRegression(random_state=0)
+
