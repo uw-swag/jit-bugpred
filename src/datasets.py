c3f6bd75adebb0f7752626fea8e00b43969d1225
Hossein Keshavarz <hosseinkeshavarz1997@gmail.com>
2020-11-25 01:42:15 +0330

    remove submodules, refactor model hugely, complete the model by adding JITGGNN, and add pytorch dataset by taking embedding from CodeBERT

--- src/datasets.py
+++ src/datasets.py
@@ -1,15 +1,18 @@
 import ast
 import json
 import os
-import traceback
-
-import numpy as np
-from torch.utils.data import DataLoader
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, Dataset
+from transformers import AutoTokenizer, AutoModel
 
 from ast_visitor import ASTVisitor
 
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
 data_path = os.path.join(BASE_PATH, 'data')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+HIDDEN_SIZE = 768
 
 
 def get_asts(filename):
@@ -22,8 +25,8 @@ def get_asts(filename):
 
     ast_dict = dict()
     for commit, files in commit_codes.items():
-        for f in files:                     # f is a tuple (name, before content, after content)
-            fname = f[0].split('/')[-1]     # path/to/«file.py»
+        for f in files:  # f is a tuple (name, before content, after content)
+            fname = f[0].split('/')[-1]  # path/to/«file.py»
             ftype = fname.split('.')[-1]
             # exclude newly added files and unsupported ones
             if f[1] == empty_template or ftype not in supported_files:
@@ -59,134 +62,75 @@ def get_asts(filename):
     return ast_dict
 
 
-def load_graphs_from_file(file_name):
-    data_list = []
-    edge_list = []
-    target_list = []
-    with open(file_name, 'r') as f:
-        for line in f:
-            if len(line.strip()) == 0:
-                data_list.append([edge_list,target_list])
-                edge_list = []
-                target_list = []
-            else:
-                digits = []
-                line_tokens = line.split(" ")
-                if line_tokens[0] == "?":
-                    for i in range(1, len(line_tokens)):
-                        digits.append(int(line_tokens[i]))
-                    target_list.append(digits)
-                else:
-                    for i in range(len(line_tokens)):
-                        digits.append(int(line_tokens[i]))
-                    edge_list.append(digits)
-    return data_list
+class ASTDataset(Dataset):
+    def __init__(self, filename, transform=None):
+        self.transform = transform
+        df = pd.read_csv(data_path + '/rawdata.csv')
+        df = df[['commit_id', 'buggy']]
+        df.set_index('commit_id', inplace=True)
+        self.label_dict = df.to_dict()['buggy']
+        with open(filename, 'r') as fp:
+            ast_dict = json.load(fp)
+        self.ast_dict = list(ast_dict.items())
+        self.tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
+        self.codebert = AutoModel.from_pretrained("microsoft/codebert-base", output_hidden_states=True)
+        self.codebert.to(device)
 
+    @staticmethod
+    def get_adjacency_matrix(n_nodes, src, dst):
+        # note that adjacency matrices in this problem are very sparse
+        a = torch.zeros(n_nodes, n_nodes)
+        for i in range(len(src)):
+            a[src[i], dst[i]] = 1
+        return a
 
-def find_max_edge_id(data_list):
-    max_edge_id = 0
-    for data in data_list:
-        edges = data[0]
-        for item in edges:
-            if item[1] > max_edge_id:
-                max_edge_id = item[1]
-    return max_edge_id
+    def get_embedding(self, file_node_tokens):
+        # more alternatives at https://github.com/BramVanroy/bert-for-inference/blob/master/introduction-to-bert.ipynb
+        # maybe not efficient
+        # will initial_representation participate in BP?
+        self.codebert.eval()
+        with torch.no_grad():
+            initial_representations = torch.zeros(len(file_node_tokens), HIDDEN_SIZE)
+            for i, node_token in enumerate(file_node_tokens):
+                hidden_states = self.codebert(self.tokenizer
+                                              .encode(node_token, return_tensors='pt')
+                                              .to(device))[2]
+                initial_representations[i, :] = torch.mean(hidden_states[-1], dim=1).squeeze()
 
-
-def find_max_node_id(data_list):
-    max_node_id = 0
-    for data in data_list:
-        edges = data[0]
-        for item in edges:
-            if item[0] > max_node_id:
-                max_node_id = item[0]
-            if item[2] > max_node_id:
-                max_node_id = item[2]
-    return max_node_id
-
-
-def find_max_task_id(data_list):
-    max_node_id = 0
-    for data in data_list:
-        targe = data[1]
-        for item in targe:
-            if item[0] > max_node_id:
-                max_node_id = item[0]
-    return max_node_id
-
-
-def split_set(data_list):
-    n_examples = len(data_list)
-    idx = range(n_examples)
-    train = idx[:50]
-    val = idx[-50:]
-    return np.array(data_list)[train], np.array(data_list)[val]
-
-
-def data_convert(data_list, n_annotation_dim):
-    n_nodes = find_max_node_id(data_list)
-    n_tasks = find_max_task_id(data_list)
-    task_data_list = []
-    for i in range(n_tasks):
-        task_data_list.append([])
-    for item in data_list:
-        edge_list = item[0]
-        target_list = item[1]
-        for target in target_list:
-            task_type = target[0]
-            task_output = target[-1]
-            annotation = np.zeros([n_nodes, n_annotation_dim])
-            annotation[target[1]-1][0] = 1
-            task_data_list[task_type-1].append([edge_list, annotation, task_output])
-    return task_data_list
-
-
-def create_adjacency_matrix(edges, n_nodes, n_edge_types):
-    a = np.zeros([n_nodes, n_nodes * n_edge_types * 2])
-    for edge in edges:
-        src_idx = edge[0]
-        e_type = edge[1]
-        tgt_idx = edge[2]
-        a[tgt_idx-1][(e_type - 1) * n_nodes + src_idx - 1] =  1
-        a[src_idx-1][(e_type - 1 + n_edge_types) * n_nodes + tgt_idx - 1] =  1
-    return a
-
-
-class bAbIDataset():
-    """
-    Load bAbI tasks for GGNN
-    """
-    def __init__(self, path, question_id, is_train):
-        all_data = load_graphs_from_file(path)
-        self.n_edge_types = find_max_edge_id(all_data)
-        self.n_tasks = find_max_task_id(all_data)
-        self.n_node = find_max_node_id(all_data)
-
-        all_task_train_data, all_task_val_data = split_set(all_data)
-
-        if is_train:
-            all_task_train_data = data_convert(all_task_train_data, 1)
-            self.data = all_task_train_data[question_id]
-        else:
-            all_task_val_data = data_convert(all_task_val_data, 1)
-            self.data = all_task_val_data[question_id]
-
-    def __getitem__(self, index):
-        am = create_adjacency_matrix(self.data[index][0], self.n_node, self.n_edge_types)
-        annotation = self.data[index][1]
-        target = self.data[index][2] - 1
-        return am, annotation, target
+        return initial_representations
 
     def __len__(self):
-        return len(self.data)
+        return len(self.ast_dict)
 
+    def __getitem__(self, item):
+        commit = self.ast_dict[item]
+        label = 1 if self.label_dict[commit[0]] else 0
+        training_data = []
+        for file in commit[1]:
+            # file is a list of 3 elements: name, before, and after. before and after are lists of two things
+            # node tokens and node edges. node tokens is a list of lists of node tokens (node -> type + token)
+            n_nodes = len(file[1][0])
+            before_tokens = self.get_embedding([' '.join(node) for node in file[1][0]])
+            after_tokens = self.get_embedding([' '.join(node) for node in file[2][0]])
+            before_adj = self.get_adjacency_matrix(n_nodes, file[1][1][0], file[1][1][1])
+            after_adj = self.get_adjacency_matrix(n_nodes, file[2][1][0], file[2][1][1])
+            training_data.append([before_tokens, before_adj, after_tokens, after_adj, label])
 
-class bAbIDataLoader(DataLoader):
-    def __init__(self, *args, **kwargs):
-        super(bAbIDataLoader, self).__init__(*args, **kwargs)
+        if self.transform is not None:
+            training_data = self.transform(training_data)
+
+        return training_data
 
 
 if __name__ == "__main__":
-    ast_dict = get_asts('source_codes_1000.json')
-    with open(data_path + '/asts_1000_synerr.json', 'w') as fp:
-        json.dump(ast_dict, fp)
+    # ast_dict = get_asts('source_codes_1000.json')
+    # with open(data_path + '/asts_1000_synerr.json', 'w') as fp:
+    #     json.dump(ast_dict, fp)
+    # with open(data_path + '/asts_300_synerr.json', 'r') as fp:
+    #     ast_dict = json.load(fp)
+    ast_dataset = ASTDataset(data_path + '/asts_300_synerr.json')
+    print(ast_dataset[0])
+    train_loader = DataLoader(ast_dataset, batch_size=1, shuffle=False)
+    train_iter = iter(train_loader)
+    data = train_iter.next()
+    print()
