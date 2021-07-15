f155df05134214caf1d807c758e79a9964f58e78
Hossein Keshavarz <hosseinkeshavarz1997@gmail.com>
2021-07-15 04:40:54 -0400

    extracted commit data

---
+++ gum.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=def-m2nagapp
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
+#SBATCH --time=00-23:00:00
+#SBATCH --mail-user=hosseinkeshavarz1997@gmail.com
+#SBATCH --mail-type=ALL
+
+source /home/hkshvrz/projects/def-m2nagapp/hkshvrz/jit-bugpred/venv/bin/activate
+which python
+module load java/11
+python -u src/gumtree.py
