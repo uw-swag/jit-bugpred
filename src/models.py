57ddf02521ddd92584ec204e421154ab0f86df8a
Hossein Keshavarz <hosseinkeshavarz1997@gmail.com>
2021-10-19 23:39:49 -0400

    fix minor bugs

--- src/models.py
+++ src/models.py
@@ -198,7 +198,7 @@ class JITGNN(nn.Module):
         self.gnn24 = GraphConvolution(message_size, message_size)
         self.attention = AttentionModule(message_size)
         self.tensor_net = TensorNetworkModule(message_size, self.neuron_size)
-        self.fc = nn.Linear(self.neuron_size, 1)
+        self.fc = nn.Linear(self.neuron_size + 6, 1)
 
     def forward(self, b_x, b_adj, a_x, a_adj, metrics):
         # change the design here. add adjacency matrix to graph convolution class so not pass it every time.
